1f1611e1514a2944b6d4c6168bec284de3896f53
Mayurapabasara <mayurapabasara143@gmail.com>
2025-11-07 21:50:41 +0530

    Refactor authentication and service layers, remove redundant classes, and update configuration

--- library-management-system/src/main/java/com/mayura/library_management_system/Services/CustomUserDetails.java
+++ library-management-system/src/main/java/com/mayura/library_management_system/Services/CustomUserDetails.java
@@ -1,5 +1,6 @@
 package com.mayura.library_management_system.Services;
 
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,24 +10,25 @@ import com.mayura.library_management_system.model.User;
 import com.mayura.library_management_system.repository.UserRepository;
 
 @Service
-public class CustomUserDetails implements UserDetailsService{
+public class CustomUserDetails implements UserDetailsService {
 
-	private UserRepository userRepo;
-	public CustomUserDetails(UserRepository userRepo) {
-		this.userRepo = userRepo;
-	}
-	  
-	
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		
-		User user = userRepo.findByUsername(username);
-		if(user == null) {
-			throw new UsernameNotFoundException("User not found with given username");
-		}
-		return org.springframework.security.core.userdetails.User.withUsername(user.getUsername()).password(user.getPassword()).roles(user.getRole()).build();
-	}
-	
+    private final UserRepository userRepo;
 
+    public CustomUserDetails(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
 }
